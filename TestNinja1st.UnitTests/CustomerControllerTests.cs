469e083f871116098e70b7a34aa6c09133bba693
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2022-09-28 11:51:55 -0500

    Testing Controllers and Types

---
+++ TestNinja1st.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja1st.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            //Not found
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //Not found or one of its derivates
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(5);
+
+            //Not found
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}

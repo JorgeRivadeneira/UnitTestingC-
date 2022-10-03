4cf9a5d21c9d8cb8d080418cc2e19dfa3a89233e
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2022-10-03 10:06:33 -0500

    Excercises

---
+++ TestNinja1st.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja1st.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(3, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(2, "2")]
+        public void GetOutput_WhenCalled_ReturnsFizzOrBuzzOrFizzBuzzOrTheNumber(int number, string expectedResults)
+        {
+
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(expectedResults));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo( "FizzBuzz"));
+        }
+    }
+}
---
+++ TestNinja1st.UnitTests/StackTests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja1st.UnitTests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        [Ignore("Is not working the throw exception method")]
+        public void Push_ArgIsNull_ThrowArgNullException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Push_ValidArg_AddTheObjectToTheStack()
+        {
+            var stack = new Stack<string>();
+
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Count_EmptyStack_ReturnZero()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_ReturnObjectOnTheTop()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Pop_StackWithFewObjects_RemoveObjectOnTheTop()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Peek_EmptyStack_ThrowInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_ReturnObjectOnTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            var result = stack.Pop();
+
+            //Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+        public void Peek_StackWithObjects_DoesNotRemoveTheObjectOnTopOfTheStack()
+        {
+            //Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            //Act
+            stack.Pop();
+
+            //Assert
+            Assert.That(stack.Count, Is.EqualTo(3));
+        }
+
+    }
+}

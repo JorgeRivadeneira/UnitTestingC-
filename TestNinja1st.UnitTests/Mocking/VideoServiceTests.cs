d3131ccb0450ffcf83c79f758a7b6e72b4debb34
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2022-10-11 13:00:11 -0500

    Using Moq Framework

--- TestNinja1st.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja1st.UnitTests/Mocking/VideoServiceTests.cs
@@ -5,19 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TestNinja.Mocking;
+using Moq;
 
 namespace TestNinja1st.UnitTests.Mocking
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new MockFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns(String.Empty);
+            //See documentation about Moq framweork in google
 
             //var result = service.ReadVideoTitle(new MockFileReader());
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
